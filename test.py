cd2c5d7325c67d1881988ad72d8eb6af985a26f6
Dominika Kusarek <dominika.kusarek@gmail.com>
2017-09-12 18:37:13 +0200

    adding travis vol13

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-from scripts.gamecomponents.tests import test_TestVector
+import unittest
+from scripts.gamecomponents.tests.test_TestPhysicsObject import TestPhysicsObject
+from scripts.gamecomponents.tests.test_TestVector import TestVector
+from scripts.gamecomponents.tests.TestDisc import TestDisc
 
-if __name__ == "__main__":
-    test_TestVector.unittest.main()
+runner = unittest.TextTestRunner()
+runner.run(unittest.makeSuite(TestVector))
+runner.run(unittest.makeSuite(TestPhysicsObject))
+runner.run(unittest.makeSuite(TestDisc))
